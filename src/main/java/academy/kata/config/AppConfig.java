89d048393e4699506e0cad6033cba9861970d8cb
Lexa <a.pisarenko9001@gmail.com>
2022-05-08 15:20:43 +0700

    EntityManager configuration without persistence.xml

--- src/main/java/academy/kata/config/AppConfig.java
+++ src/main/java/academy/kata/config/AppConfig.java
@@ -1,32 +1,67 @@
 package academy.kata.config;
 
+import com.mysql.jdbc.Driver;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
+@PropertySource("classpath:db.properties")
 @ComponentScan(basePackages = {"academy.kata.service", "academy.kata.dao"})
 public class AppConfig {
 
-    private Environment env;
+    private final Environment environment;
 
-
-    public AppConfig(Environment env) {
-        this.env = env;
+    public AppConfig(Environment environment) {
+        this.environment = environment;
     }
 
     @Bean
-    public EntityManagerFactory getEntityManagerFactory(){
-        return Persistence.createEntityManagerFactory("academy.kata.mySql");
+    public DataSource getDataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setUsername(environment.getProperty("user"));
+        dataSource.setPassword(environment.getProperty("password"));
+        dataSource.setUrl(environment.getProperty("url"));
+        dataSource.setDriverClass(Driver.class);
+        return dataSource;
+    }
+
+    @Bean("LocalContainerEntityManagerFactoryBean")
+    public LocalContainerEntityManagerFactoryBean getEntityManagerFactory(DataSource dataSource) {
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setJpaProperties(properties);
+        emf.setPackagesToScan("academy.kata.model");
+        emf.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        emf.afterPropertiesSet();
+
+        return emf;
     }
 
     @Bean
-    public EntityManager getEntityManager(EntityManagerFactory entityManagerFactory) {
+    public EntityManagerFactory getEntityManagerFactory(LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean) {
+        return localContainerEntityManagerFactoryBean.getObject();
+    }
+
+    @Bean
+    public EntityManager getEntityManager(@Autowired @Qualifier("LocalContainerEntityManagerFactoryBean") EntityManagerFactory entityManagerFactory) {
         return entityManagerFactory.createEntityManager();
     }
 
--- src/test/java/config/SpringTestConfig.java
+++ src/test/java/config/SpringTestConfig.java
@@ -1,24 +1,65 @@
 package config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.h2.jdbcx.JdbcDataSource;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
+@PropertySource("classpath:db.properties")
 @ComponentScan(value = "academy.kata.dao")
 public class SpringTestConfig {
 
-    @Bean
-    public EntityManagerFactory getEntityManagerFactory(){
-        return Persistence.createEntityManagerFactory("academy.kata.h2");
+    @Autowired
+    private final Environment environment;
+
+    public SpringTestConfig(Environment environment) {
+        this.environment = environment;
     }
 
     @Bean
-    public EntityManager getEntityManager(EntityManagerFactory entityManagerFactory) {
+    public DataSource getDataSource() {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUser(environment.getProperty("user"));
+        dataSource.setPassword(environment.getProperty("password"));
+        dataSource.setURL(environment.getProperty("url"));
+        return dataSource;
+    }
+
+    @Bean("LocalContainerEntityManagerFactoryBean")
+    public LocalContainerEntityManagerFactoryBean getEm(DataSource dataSource) {
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+
+
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setJpaProperties(properties);
+        emf.setPackagesToScan("academy.kata.model");
+        emf.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        emf.afterPropertiesSet();
+
+        return emf;
+    }
+
+    @Bean
+    public EntityManagerFactory getEntityManagerFactory(LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean) {
+        return localContainerEntityManagerFactoryBean.getObject();
+    }
+
+    @Bean
+    public EntityManager getEntityManager(@Autowired @Qualifier("LocalContainerEntityManagerFactoryBean") EntityManagerFactory entityManagerFactory) {
         return entityManagerFactory.createEntityManager();
     }
 }
